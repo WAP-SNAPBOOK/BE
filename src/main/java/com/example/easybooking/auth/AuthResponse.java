6bc751098bec342007ee3326d1006f78b39b614d
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-09-22 23:03:36 +0900

    #12 [FEATURE] 로그인 시, 미가입 유저 판별 로직 구현

--- src/main/java/com/example/easybooking/auth/AuthResponse.java
+++ src/main/java/com/example/easybooking/auth/AuthResponse.java
@@ -10,19 +10,21 @@ public class AuthResponse {
     private String refreshToken;
     private String role;
     private String message;
+    private boolean isNewUser;
 
-    public static AuthResponse success(String accessToken, String refreshToken, String role) {
+    public static AuthResponse success(String accessToken, String refreshToken, String role, boolean isNewUser) {
         return AuthResponse.builder()
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .role(role)
                 .message("로그인 성공")
+                .isNewUser(isNewUser)
                 .build();
     }
     public static AuthResponse failure(String message) {
-        return AuthResponse.builder()
-                .message(message)
-                .build();
+            return AuthResponse.builder()
+                    .message(message)
+                    .build();
     }
 }
 
--- src/main/java/com/example/easybooking/auth/AuthService.java
+++ src/main/java/com/example/easybooking/auth/AuthService.java
@@ -1,5 +1,7 @@
 package com.example.easybooking.auth;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.example.easybooking.user.User;
@@ -22,14 +24,18 @@ public class AuthService {
     public AuthResponse oAuthLogin(String accessCode, HttpServletResponse httpServletResponse) {
         try{
             KakaoDto.KakaoId kakaoId = kakaoUtil.requestKakaoId(accessCode);
-            User user = userReader.getUserByKakaoId(String.valueOf(kakaoId.getId()))
-                    .orElseGet(() -> createNewUser(kakaoId.getId()));
+            Optional<User> existingUser = userReader.getUserByKakaoId(String.valueOf(kakaoId.getId()));
+            boolean isNewUser = existingUser.isEmpty();
+            User user = existingUser.orElseGet(() -> createNewUser(kakaoId.getId()));
+
             String accessToken = jwtUtil.generateAccessToken(String.valueOf(user.getKakaoId()), user.getRole().name());
             String refreshToken = jwtUtil.generateRefreshToken(String.valueOf(user.getKakaoId()));
+
             return AuthResponse.success(
                     accessToken,
                     refreshToken,
-                    user.getRole().name()
+                    user.getRole().name(),
+                    isNewUser
             );
         }
         catch (Exception e){
@@ -38,7 +44,6 @@ public class AuthService {
         }
 
     }
-
 
     private User createNewUser(Long kakaoId) {
         User newUser = User.createNewUser(String.valueOf(kakaoId));
