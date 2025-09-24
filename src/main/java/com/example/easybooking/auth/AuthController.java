22e09226c15f57805af0f8046abee2f24dce0c48
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-09-24 14:56:33 +0900

    #12 [REFACTOR] 회원가입 api 구현

--- src/main/java/com/example/easybooking/auth/AuthController.java
+++ src/main/java/com/example/easybooking/auth/AuthController.java
@@ -1,14 +1,13 @@
 package com.example.easybooking.auth;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
---
+++ src/main/java/com/example/easybooking/user/dto/SignUpResponse.java
@@ -0,0 +1,12 @@
+package com.example.easybooking.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SignUpResponse {
+    private String nickname;
+}
