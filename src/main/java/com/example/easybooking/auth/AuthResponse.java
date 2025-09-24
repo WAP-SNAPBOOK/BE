9d19f53a50aee53378fe34682ac2091ac3c89cb8
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-09-24 14:56:14 +0900

    #12 [REFACTOR] OAuth 로그인 응답 개선

--- src/main/java/com/example/easybooking/auth/AuthResponse.java
+++ src/main/java/com/example/easybooking/auth/AuthResponse.java
@@ -10,27 +10,33 @@ public class AuthResponse {
     private String refreshToken;
     private String role;
     private String message;
-    private boolean isNewUser;
+    private AuthStatus authStatus;
 
     public static AuthResponse loginSuccess(String accessToken, String refreshToken, String role) {
         return AuthResponse.builder()
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .role(role)
+                .authStatus(AuthStatus.LOGIN_SUCCESS)
                 .message("로그인 성공")
                 .build();
     }
     public static AuthResponse signupRequired(String tempToken) {
         return AuthResponse.builder()
                 .accessToken(tempToken)
+                .authStatus(AuthStatus.SIGNUP_REQUIRED)
                 .message("회원가입 필요")
-                .isNewUser(true)
                 .build();
     }
     public static AuthResponse failure(String message) {
             return AuthResponse.builder()
                     .message(message)
                     .build();
+    }
+
+    public enum AuthStatus {
+        LOGIN_SUCCESS,      // 기존 회원 로그인
+        SIGNUP_REQUIRED     // 회원가입 필요
     }
 }
 
