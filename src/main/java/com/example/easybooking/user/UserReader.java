eaee42791ec5e9507e8e89897981310df85c86e4
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-09-25 11:31:45 +0900

    #12 [REFACTOR] 사용자 조회 클래스 통합

--- src/main/java/com/example/easybooking/user/UserReader.java
+++ src/main/java/com/example/easybooking/user/UserReader.java
@@ -1,5 +1,9 @@
 package com.example.easybooking.user;
 
+import com.example.easybooking.user.domain.User;
+import com.example.easybooking.user.domain.repository.CustomerProfileRepository;
+import com.example.easybooking.user.domain.repository.OwnerProfileRepository;
+import com.example.easybooking.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +13,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserReader {
     private final UserRepository userRepository;
+    private final CustomerProfileRepository customerProfileRepository;
+    private final OwnerProfileRepository ownerProfileRepository;
 
     public Optional<User> getUserByKakaoId(String kakaoId) {
         return userRepository.findByKakaoId(kakaoId);
