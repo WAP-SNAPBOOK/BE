5e44d34d38dbb2fd9b722964201cf96c4cbc7ce8
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-09-25 13:50:14 +0900

    #12 [CHORE] swagger authorize 추가

--- src/main/java/com/example/easybooking/config/OpenApiConfig.java
+++ src/main/java/com/example/easybooking/config/OpenApiConfig.java
@@ -2,13 +2,15 @@ package com.example.easybooking.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "Snapbook API", version = "v1", description = "Snapbook API 문서")
+        info = @Info(title = "Snapbook API", version = "v1", description = "Snapbook API 문서"),
+        security = { @SecurityRequirement(name = "bearerAuth") }
 )
 @SecurityScheme(
         name = "bearerAuth",
