53d2d077b3a1f863bd6924a692e69326adfe6992
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-10-02 20:07:34 +0900

    #24 [REFACTOR] User 저장 로직에 트랜잭션 부여

--- src/main/java/com/example/easybooking/user/UserWriter.java
+++ src/main/java/com/example/easybooking/user/UserWriter.java
@@ -3,6 +3,7 @@ package com.example.easybooking.user;
 import com.example.easybooking.user.domain.CustomerProfile;
 import com.example.easybooking.user.domain.OwnerProfile;
 import com.example.easybooking.user.domain.User;
+import com.example.easybooking.user.domain.UserType;
 import com.example.easybooking.user.domain.repository.CustomerProfileRepository;
 import com.example.easybooking.user.domain.repository.OwnerProfileRepository;
 import com.example.easybooking.user.domain.repository.UserRepository;
@@ -10,6 +11,7 @@ import com.example.easybooking.user.dto.CustomerSignUpRequest;
 import com.example.easybooking.user.dto.OwnerSignUpRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -21,22 +23,32 @@ public class UserWriter {
         return userRepository.save(user);
     }
 
+    @Transactional
     public User registerCustomer(CustomerSignUpRequest request,String providerId) {
-        User user = User.createUser(providerId, request.getNickname());
+        User user = User.createUser(providerId, request.getNickname(), UserType.CUSTOMER);
+        User savedUser = userRepository.save(user);
+
         CustomerProfile customerProfile = CustomerProfile.createCustomerProfile(providerId);
         customerProfileRepository.save(customerProfile);
-        return userRepository.save(user);
+
+        return savedUser;
     }
 
+    @Transactional
     public User registerOwner(OwnerSignUpRequest request, String providerId) {
-        User user = User.createUser(providerId, request.getNickname());
+        User user = User.createUser(providerId, request.getNickname(), UserType.OWNER);
+        User savedUser = userRepository.save(user);
+
         OwnerProfile ownerProfile = OwnerProfile.createOwnerProfile(
-                providerId,request.getBusinessName(),
+                providerId,
+                request.getBusinessName(),
                 request.getBusinessNumber(),
                 request.getAddress(),
-                request.getPhoneNumber());
+                request.getPhoneNumber()
+        );
         ownerProfileRepository.save(ownerProfile);
-        return userRepository.save(user);
+
+        return savedUser;
     }
 
     public void deleteUser(String providerId) {
