3b332421eae6a2952f0f841eb04182b1e4d98887
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-10-02 20:22:40 +0900

    #24 [REFACTOR] User 엔티티에 UserType 추가 및 회원가입 응답 구조 개선

--- src/main/java/com/example/easybooking/user/service/UserService.java
+++ src/main/java/com/example/easybooking/user/service/UserService.java
@@ -3,8 +3,11 @@ package com.example.easybooking.user.service;
 
 import com.example.easybooking.auth.AuthTokens;
 import com.example.easybooking.auth.JwtUtil;
+import com.example.easybooking.user.UserReader;
 import com.example.easybooking.user.UserWriter;
+import com.example.easybooking.user.domain.OwnerProfile;
 import com.example.easybooking.user.domain.User;
+import com.example.easybooking.user.domain.UserType;
 import com.example.easybooking.user.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,30 +16,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserWriter userWriter;
+    private final UserReader userReader;
     private final JwtUtil jwtUtil;
 
     public CustomerSignUpResponse signUpCustomer(String providerId, CustomerSignUpRequest request) {
         User savedUser = userWriter.registerCustomer(request, providerId);
         AuthTokens tokens = jwtUtil.generateTokens(providerId, savedUser.getRole().name());
 
-        return CustomerSignUpResponse.builder()
-                .nickname(savedUser.getNickname())
-                .tokens(tokens)
-                .build();
+        return CustomerSignUpResponse.of(UserType.CUSTOMER,savedUser, tokens);
     }
 
     public OwnerSignUpResponse signUpOwner(String providerId, OwnerSignUpRequest request) {
         User savedUser = userWriter.registerOwner(request, providerId);
+        OwnerProfile profile = userReader.getOwnerProfile(providerId);
         AuthTokens tokens = jwtUtil.generateTokens(providerId, savedUser.getRole().name());
-
-        return OwnerSignUpResponse.builder()
-                .businessNumber(request.getBusinessNumber())
-                .address(request.getAddress())
-                .phoneNumber(request.getPhoneNumber())
-                .businessName(request.getBusinessName())
-                .nickname(savedUser.getNickname())
-                .tokens(tokens)
-                .build();
+        return OwnerSignUpResponse.of(UserType.OWNER,savedUser,tokens,profile);
     }
     public void deleteUser(String providerId) {
         userWriter.deleteUser(providerId);
