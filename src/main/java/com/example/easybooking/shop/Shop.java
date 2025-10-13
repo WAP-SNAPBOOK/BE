52d90fb05ca69cc59bed9b80f3a39cf5f7eb78fe
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-10-13 13:24:17 +0900

    #18 [CHORE] 잡다한 수정

--- src/main/java/com/example/easybooking/shop/Shop.java
+++ src/main/java/com/example/easybooking/shop/Shop.java
@@ -1,15 +1,21 @@
 package com.example.easybooking.shop;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Shop {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private Long OwnerId;
+    private Long ownerId;
 
     @Column(nullable = false)
     private String businessName;
