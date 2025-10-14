c6d1ad6be4f1825eb58d6a7d9678eed5b5b8ed35
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-10-14 14:01:27 +0900

    #18 [FIX] User 엔티티 수정으로 인한 dto 수정

--- src/main/java/com/example/easybooking/user/dto/OwnerSignUpResponse.java
+++ src/main/java/com/example/easybooking/user/dto/OwnerSignUpResponse.java
@@ -14,13 +14,15 @@ import lombok.NoArgsConstructor;
 @Builder
 public class OwnerSignUpResponse {
     private UserType userType;
-    private String nickname;
+    private String name;
+    private String phoneNumber;
     private AuthTokens tokens;
 
     public static OwnerSignUpResponse of(UserType userType,User user, AuthTokens tokens) {
         return OwnerSignUpResponse.builder()
                 .userType(userType)
-                .nickname(user.getNickname())
+                .name(user.getName())
+                .phoneNumber(user.getPhoneNumber())
                 .tokens(tokens)
                 .build();
     }
