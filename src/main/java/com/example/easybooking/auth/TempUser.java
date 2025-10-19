4aba12c82955da9d649523816ff78fca775b6b22
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-10-19 13:39:23 +0900

    #34 [REFACTOR] 도메인 모델 추가

---
+++ src/main/java/com/example/easybooking/auth/TempUser.java
@@ -0,0 +1,17 @@
+package com.example.easybooking.auth;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@RequiredArgsConstructor
+@ToString
+public final class TempUser implements AuthPrincipal {
+    private final String providerId;
+    
+    @Override
+    public boolean isTemporary() {
+        return true;
+    }
+}
