30e79a0198078e61b5b5b6d7542054efa8395944
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-10-21 14:49:34 +0900

    #36 [FIX] UserType.Owner만 shop 생성할 수 있도록 제한

--- src/main/java/com/example/easybooking/shop/ShopWriter.java
+++ src/main/java/com/example/easybooking/shop/ShopWriter.java
@@ -4,6 +4,9 @@ import com.example.easybooking.shop.domain.Shop;
 import com.example.easybooking.shop.dto.CreateShopRequest;
 import com.example.easybooking.shop.dto.CreateShopResponse;
 import com.example.easybooking.shop.repository.ShopRepository;
+import com.example.easybooking.user.UserReader;
+import com.example.easybooking.user.domain.User;
+import com.example.easybooking.user.domain.UserType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,15 +14,20 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ShopWriter {
     private final ShopRepository shopRepository;
-    private final ShopReader shopreader;
+    private final ShopReader shopReader;
+    private final UserReader userReader;
 
     public Shop save(Shop shop) {
         return shopRepository.save(shop);
     }
 
     public CreateShopResponse create(Long ownerId, CreateShopRequest request) {
-        if(shopreader.isExist(ownerId)){
+        if(shopReader.isExist(ownerId)){
             throw new IllegalStateException("이미 매장을 등록한 사용자입니다");
+        }
+        User user = userReader.read(ownerId);
+        if(user.getUserType() != UserType.OWNER){
+            throw new IllegalStateException("점주 사용자가 아니면 매장을 등록할 수 없습니다.");
         }
         Shop shop = Shop.create(ownerId,request);
         shopRepository.save(shop);
