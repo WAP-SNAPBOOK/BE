a652eb1746e305209631823a9b8f99c278e05932
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-10-22 23:25:17 +0900

    #38 [REFACTOR] Message 엔티티 비정규화

--- src/main/java/com/example/easybooking/chat/domain/Message.java
+++ src/main/java/com/example/easybooking/chat/domain/Message.java
@@ -20,16 +20,20 @@ public class Message {
     @Column(nullable = false)
     private Long senderId;
 
+    @Column(nullable = false)
+    private String senderName;
+
     @Column(columnDefinition = "TEXT")
     private String content;
 
     @Column(nullable = false, updatable = false)
     private LocalDateTime sentAt;
 
-    public static Message create(Long chatRoomId,Long senderId, String content) {
+    public static Message create(Long chatRoomId,Long senderId,String senderName, String content) {
         Message message = new Message();
         message.chatRoomId = chatRoomId;
         message.senderId = senderId;
+        message.senderName = senderName;
         message.content = content;
         message.sentAt = LocalDateTime.now();
         return message;
