b256ed13d33cf292ebbc3777b7648feea4156df3
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-10-30 14:38:59 +0900

    #51 [FIX] Shop 엔티티에 publiCode, slug 추가

--- src/main/java/com/example/easybooking/shop/repository/ShopRepository.java
+++ src/main/java/com/example/easybooking/shop/repository/ShopRepository.java
@@ -3,6 +3,10 @@ package com.example.easybooking.shop.repository;
 import com.example.easybooking.shop.domain.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+    Optional<Shop> findByPublicCode(String publicCode);
+    Optional<Shop> findBySlug(String slug);
     boolean existsByOwnerId(Long ownerId);
 }
