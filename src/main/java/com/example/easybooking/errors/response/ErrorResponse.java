d2c56ba700a0c7af63263d0a8a49928b85412415
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-11-10 13:50:26 +0900

    #57 [FEATURE] 예외 처리 메서드 구현

--- src/main/java/com/example/easybooking/errors/response/ErrorResponse.java
+++ src/main/java/com/example/easybooking/errors/response/ErrorResponse.java
@@ -1,10 +1,12 @@
 package com.example.easybooking.errors.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
+@Builder
 public class ErrorResponse {
     private final String code;
     private final String message;
