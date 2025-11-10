f8153021157e276c5a0a9fd4d7f0ec93735a6465
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-11-10 13:50:26 +0900

    #57 [FEATURE] 예외 처리 메서드 구현

--- src/main/java/com/example/easybooking/errors/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/easybooking/errors/handler/GlobalExceptionHandler.java
@@ -1,9 +1,21 @@
 package com.example.easybooking.errors.handler;
 
+import com.example.easybooking.errors.errorcode.ErrorCode;
+import com.example.easybooking.errors.response.ErrorResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
+    private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(makeErrorResponse(errorCode));
+    }
+    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .build();
+    }
 }
