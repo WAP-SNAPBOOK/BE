20d94cc0c54d928537ac0574305c63906fe002b6
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-11-10 14:57:46 +0900

    #57 [FEATURE] KakaoUtil 예외 처리 적용

--- src/main/java/com/example/easybooking/errors/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/easybooking/errors/handler/GlobalExceptionHandler.java
@@ -1,18 +1,27 @@
 package com.example.easybooking.errors.handler;
 
+import com.example.easybooking.errors.errorcode.AuthErrorCode;
 import com.example.easybooking.errors.errorcode.ErrorCode;
+import com.example.easybooking.errors.exception.AuthException;
 import com.example.easybooking.errors.response.ErrorResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode) {
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<ErrorResponse> handleAuthException(AuthException e) {
+        AuthErrorCode authErrorCode = e.getAuthErrorCode();
+        return handleExceptionInternal(authErrorCode);
+    }
+    public ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode) {
         return ResponseEntity.status(errorCode.getHttpStatus())
                 .body(makeErrorResponse(errorCode));
     }
-    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+    public ErrorResponse makeErrorResponse(ErrorCode errorCode) {
         return ErrorResponse.builder()
                 .code(errorCode.name())
                 .message(errorCode.getMessage())
