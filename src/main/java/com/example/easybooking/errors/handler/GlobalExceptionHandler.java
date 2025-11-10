133cca33940dd31d981b73e89f2e4ce691ed23b4
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-11-10 13:18:25 +0900

    #57 [FEATURE] 전역 예외 처리 구성

---
+++ src/main/java/com/example/easybooking/errors/handler/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.example.easybooking.errors.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
