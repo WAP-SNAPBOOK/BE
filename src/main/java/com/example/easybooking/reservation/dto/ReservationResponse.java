b0c56a87496eac3d48d643abc5fc0fbca05652f3
oiuyzin <jhj030612@naver.com>
2025-11-13 13:47:38 +0900

    [FEATURE] 예약 생성 응답 확장
    고객의 예약 접수 완료 화면에 필요한 모든 상세 정보(폼 데이터)를 ReservationResponse DTO에 포함시키도록 응답 구조를 확장함.

--- src/main/java/com/example/easybooking/reservation/dto/ReservationResponse.java
+++ src/main/java/com/example/easybooking/reservation/dto/ReservationResponse.java
@@ -4,6 +4,7 @@ import com.example.easybooking.reservation.domain.Reservation;
 import lombok.Data;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 @Data
 public class ReservationResponse {
@@ -12,13 +13,49 @@ public class ReservationResponse {
     private LocalTime time;
     private Reservation.Status status;
 
-    // 추가로 필요한 정보 ?
-    // 예약 시각, 샵 이름 등
+    // 추가로 필요한 정보
+    private String customerName;
+    private Integer photoCount;
 
-    public ReservationResponse(Reservation entity) {
+    private String part;
+    private String removal;
+    private Integer extendCount;
+    private Integer wrappingCount;
+    private String extendStatus;     // 연장 유/무 (화면 표시용)
+    private String wrappingStatus;   // 래핑 유/무 (화면 표시용)
+    private List<String> photoUrls;
+    private String requests;
+
+
+    public ReservationResponse(
+            Reservation entity,
+            String customerName,
+            Integer photoCount,
+            String part,
+            String removal,
+            Integer extendCount,
+            Integer wrappingCount,
+            List<String> photoUrls,
+            String requests
+    ) {
         this.id = entity.getId();
         this.date = entity.getDate();
         this.time = entity.getTime();
         this.status = entity.getStatus();
+
+        this.customerName = customerName;
+        this.photoCount = photoCount;
+
+        this.part = part;
+        this.removal = removal;
+        this.requests = requests;
+        this.photoUrls = photoUrls;
+
+        this.extendCount = extendCount;
+        this.wrappingCount = wrappingCount;
+
+        // 수량을 기반으로 '유/무' 상태로 바꾸기
+        this.extendStatus = (extendCount != null && extendCount > 0) ? "유" : "무";
+        this.wrappingStatus = (wrappingCount != null && wrappingCount > 0) ? "유" : "무";
     }
 }
