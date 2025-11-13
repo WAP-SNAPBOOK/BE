c36060b7ff702197cee3cdba00f6f20255d05c91
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-11-13 15:26:31 +0900

    hotfix : UserErrorCode 주석처리

--- src/main/java/com/example/easybooking/errors/errorcode/UserErrorCode.java
+++ src/main/java/com/example/easybooking/errors/errorcode/UserErrorCode.java
@@ -1,12 +1,12 @@
-package com.example.easybooking.errors.errorcode;
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-
-@RequiredArgsConstructor
-@Getter
-public enum UserErrorCode implements ErrorCode {
-    private final HttpStatus httpStatus;
-    private final String message;
-}
+//package com.example.easybooking.errors.errorcode;
+//
+//import lombok.Getter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//
+//@RequiredArgsConstructor
+//@Getter
+//public enum UserErrorCode implements ErrorCode {
+//    private final HttpStatus httpStatus;
+//    private final String message;
+//}
