3dc812db16c31a77ef37c0f8966a9032eebe38df
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-11-14 15:23:01 +0900

    #68 [REFACTOR] Form 도메인에 대한 예외처리 구현

--- src/main/java/com/example/easybooking/form/FormFieldReader.java
+++ src/main/java/com/example/easybooking/form/FormFieldReader.java
@@ -1,26 +1,27 @@
 package com.example.easybooking.form;
 
-import java.util.List;
-
-import org.springframework.stereotype.Component;
-
+import com.example.easybooking.errors.errorcode.FormErrorCode;
+import com.example.easybooking.errors.exception.FormException;
 import com.example.easybooking.form.domain.Form;
 import com.example.easybooking.form.domain.FormField;
 import com.example.easybooking.form.domain.repository.FormFieldRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class FormFieldReader {
 
     private final FormFieldRepository formFieldRepository;
-    public List<FormField> read(Form form){
+
+    public List<FormField> read(Form form) {
         return formFieldRepository.findByFormWithOptions(form);
     }
 
-    public FormField readByFormAndFieldId(Form form, String fieldId){
+    public FormField readByFormAndFieldId(Form form, String fieldId) {
         return formFieldRepository.findByFormAndFieldId(form, fieldId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 필드를 찾을 수 없습니다: " + fieldId));
+                .orElseThrow(() -> new FormException(FormErrorCode.FORM_FIELD_NOT_FOUND));
     }
 }
