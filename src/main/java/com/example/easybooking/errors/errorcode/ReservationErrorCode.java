d8a707b0b07d29e0aded8cf27e9f02ce3c9f5708
oiuyzin <jhj030612@naver.com>
2025-11-15 00:14:13 +0900

    [FIX] 예약 생성 시 이중 예약 방지
    - 동시에 들어오는 예약 생성 요청으로 인해 이중 예약이 발생하는 문제 해결.

--- src/main/java/com/example/easybooking/errors/errorcode/ReservationErrorCode.java
+++ src/main/java/com/example/easybooking/errors/errorcode/ReservationErrorCode.java
@@ -13,8 +13,8 @@ public enum ReservationErrorCode implements ErrorCode {
     INVALID_FORM_JSON(HttpStatus.BAD_REQUEST, "폼 데이터 처리 중 JSON 변환 오류가 발생했습니다."),
     REQUIRED_DATE_MISSING(HttpStatus.BAD_REQUEST, "예약 날짜(date)는 필수 항목입니다."),
     REQUIRED_TIME_MISSING(HttpStatus.BAD_REQUEST, "예약 시간(time)은 필수 항목입니다."),
-    INVALID_PHOTO_JSON(HttpStatus.BAD_REQUEST, "첨부 사진(photo) 데이터 형식이 올바르지 않습니다. JSON 배열 형식이어야 합니다.");
-
+    INVALID_PHOTO_JSON(HttpStatus.BAD_REQUEST, "첨부 사진(photo) 데이터 형식이 올바르지 않습니다. JSON 배열 형식이어야 합니다."),
+    TIME_SLOT_ALREADY_BOOKED(HttpStatus.CONFLICT, "선택하신 시간은 이미 예약되었거나 접수 대기 중입니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
