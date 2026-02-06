7ee103672b752ae6928105ba1f897a2ecbb9e0a5
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2026-02-06 15:42:28 +0900

    refactor(staff): query default staff by shopId and name
    Refs: #97

--- src/main/java/com/example/easybooking/staff/StaffReader.java
+++ src/main/java/com/example/easybooking/staff/StaffReader.java
@@ -1,6 +1,7 @@
 package com.example.easybooking.staff;
 
 import com.example.easybooking.staff.domain.Staff;
+import com.example.easybooking.staff.exception.StaffNotFoundException;
 import com.example.easybooking.staff.repository.StaffRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -11,13 +12,15 @@ import org.springframework.stereotype.Component;
 public class StaffReader {
 
     private final StaffRepository staffRepository;
+    private static final String DEFAULT_STAFF_NAME = "기본";
 
     public List<Staff> findByShopId(Long shopId) {
         return staffRepository.findByShopId(shopId);
     }
 
     public Staff getDefaultStaffByShopId(Long shopId) {
-        throw new UnsupportedOperationException("TODO");
+        return staffRepository.findByShopIdAndName(shopId, DEFAULT_STAFF_NAME)
+                .orElseThrow(StaffNotFoundException::new);
     }
 }
 
