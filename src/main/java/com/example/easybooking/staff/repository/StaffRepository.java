a7f4b06e23e1e74968c384066e4e0aabefeb8523
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2026-02-06 15:42:28 +0900

    refactor(staff): query default staff by shopId and name
    Refs: #97

--- src/main/java/com/example/easybooking/staff/repository/StaffRepository.java
+++ src/main/java/com/example/easybooking/staff/repository/StaffRepository.java
@@ -2,9 +2,12 @@ package com.example.easybooking.staff.repository;
 
 import com.example.easybooking.staff.domain.Staff;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StaffRepository extends JpaRepository<Staff, Long> {
     List<Staff> findByShopId(Long shopId);
+
+    Optional<Staff> findByShopIdAndName(Long shopId, String name);
 }
 
